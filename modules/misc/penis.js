2899f4766b92e179bd790e425ead2bd11b1f4ce0
Ak <79897291+0vm@users.noreply.github.com>
2022-05-03 16:07:09 +1000

    bruh

--- modules/misc/penis.js
+++ modules/misc/penis.js
@@ -6,23 +6,21 @@ module.exports.run = (client, msg, args) => {
     let user = msg.mentions.users.first()
     if(!user) user = msg.author
     let replies = [
-        "**8=D**\n\nFucking Small 1 inch",
+        "**8=D**\n\nHas A Good Personality",
         "**8==D**\n\nSmall 2 inches",
         "**8===D**\n\nSmall But Okay 3 inches",
         "**8====D**\n\nNot Bad 4 inches",
         "**8=====D**\n\nNice 5 inches",
         "**8======D**\n\nInsane 6 inches",
          ]
-     
-         let random = Math.floor(Math.random() * replies.length)
-     
-         let embed = new RichEmbed()
-         .setTitle(`${user.tag}`)
-         .setDescription(`${replies[random]}`)
-         .setColor(config.embed_colour)
-         .setFooter(config.embed_footer)
-         msg.channel.send(embed)
-
+    let result = Math.floor((Math.random() * replies.length));
+    let embed = new RichEmbed()
+    .setAuthor(user.tag, user.displayAvatarURL)
+    .setColor(config.color)
+    .setDescription(replies[result])
+    .setFooter(config.copy)
+    .setTimestamp()
+    msg.channel.send(embed)
 }
 
 
